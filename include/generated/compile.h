7fa167023cdffca373617ada674f0c8b71737c62
dragonnn <dragonn@op.pl>
2012-05-31 14:03:44 +0200

    Merge from offical-gb

--- include/generated/compile.h
+++
@@ -1,9 +0,0 @@
-/* This file is auto generated, version 42 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#42 PREEMPT Thu May 31 13:19:38 CEST 2012"
-#define LINUX_COMPILE_TIME "13:19:38"
-#define LINUX_COMPILE_BY "dragonn"
-#define LINUX_COMPILE_HOST "arch"
-#define LINUX_COMPILE_DOMAIN "localdomain"
-#define LINUX_COMPILER "gcc version 4.5.2 (Sourcery G++ Lite 2011.03-42) "
