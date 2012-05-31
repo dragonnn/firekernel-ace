a3601ff58189aa7b567b3b0b4745ab3abd6f9703
dragonnn <dragonn@op.pl>
2012-05-31 13:58:19 +0200

    Update ramdisk to match from official CM - now everthing works

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 30 */
+/* This file is auto generated, version 42 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#30 PREEMPT Tue May 29 17:20:22 CEST 2012"
-#define LINUX_COMPILE_TIME "17:20:22"
+#define UTS_VERSION "#42 PREEMPT Thu May 31 13:19:38 CEST 2012"
+#define LINUX_COMPILE_TIME "13:19:38"
 #define LINUX_COMPILE_BY "dragonn"
 #define LINUX_COMPILE_HOST "arch"
 #define LINUX_COMPILE_DOMAIN "localdomain"
