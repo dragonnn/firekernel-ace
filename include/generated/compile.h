35ff8b42614451b707e5488928522129a6499877
dragonnn <dragonn@op.pl>
2012-06-20 14:53:58 +0200

    Update battery driver, add 540Mhz step and changes in power_supply driver

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 163 */
+/* This file is auto generated, version 167 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#163 PREEMPT Tue Jun 12 17:55:29 CEST 2012"
-#define LINUX_COMPILE_TIME "17:55:29"
+#define UTS_VERSION "#167 PREEMPT Wed Jun 20 14:50:11 CEST 2012"
+#define LINUX_COMPILE_TIME "14:50:11"
 #define LINUX_COMPILE_BY "dragonn"
 #define LINUX_COMPILE_HOST "arch"
 #define LINUX_COMPILE_DOMAIN "localdomain"
