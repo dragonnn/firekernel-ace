08bed15a18d7b1b93fc8344361aafeac44530296
dragonnn <dragonn@op.pl>
2012-08-19 20:13:56 +0200

    Build 19.08.2012

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,9 +1,9 @@
-/* This file is auto generated, version 267 */
+/* This file is auto generated, version 269 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#267 PREEMPT Sat Aug 18 21:36:29 CEST 2012"
-#define LINUX_COMPILE_TIME "21:36:29"
+#define UTS_VERSION "#269 PREEMPT Sun Aug 19 20:08:31 CEST 2012"
+#define LINUX_COMPILE_TIME "20:08:31"
 #define LINUX_COMPILE_BY "dragonn"
 #define LINUX_COMPILE_HOST "arch"
 #define LINUX_COMPILE_DOMAIN "localdomain"
-#define LINUX_COMPILER "gcc version 4.7.1 20120531 (prerelease) (crosstool-NG linaro-1.13.1-2012.06-20120625 - Linaro GCC 2012.06) "
+#define LINUX_COMPILER "gcc version 4.7.2 20120701 (prerelease) (crosstool-NG linaro-1.13.1-2012.07-20120720 - Linaro GCC 2012.07) "
